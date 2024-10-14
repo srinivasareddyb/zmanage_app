f09994fc8897b3b44b46ed0647ac038c28cd05cd
srinivasareddyb <srinivasareddy1995@gmail.com>
2024-10-14 13:03:53 +0000

    First Commit

---
+++ uimanage/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.managed.uimanage.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
